07c9a8d3fe9f67226ba7906e75a4e0be6fbdade6
Glenda Martello <glemartello@hotmail.com>
2022-10-05 15:38:25 -0300

    styling sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,16 @@
 import { Header } from "./components/Header";
 import { Post } from "./Post";
 
-import "./styles.css";
+import styles from "./App.module.css";
+
+import "./global.css";
 
 export function App() {
   return (
     <div>
       <Header />
-      <Post
-        author="Glenda Martello"
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque ea velit iure iste beatae hic aspernatur reiciendis aliquid amet saepe atque, porro dolor cumque voluptatem assumenda! Dignissimos ipsa dolores maiores."
-      />
-      <Post
-        author="Alexandre Stapenhorst"
-        content="Lorem ipsum, dolor sit amet consectetur adipisicing elit. Itaque ea velit iure iste beatae hic aspernatur reiciendis aliquid amet saepe atque, porro dolor cumque voluptatem assumenda! Dignissimos ipsa dolores maiores."
-      />
+
+      <div className="styles.wrapper"></div>
     </div>
   );
 }
