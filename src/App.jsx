e8ad1c33bab5193b5aad0d3c44c1124adaa0e338
Glenda Martello <glemartello@hotmail.com>
2022-10-18 16:16:37 -0300

    initialized post area and add styles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import { Header } from "./components/Header";
-import { Post } from "./Post";
-
-import "./global.css";
+import { Post } from "./components/Post";
+import { Sidebar } from "./components/Sidebar";
 
 import styles from "./App.module.css";
-import { Sidebar } from "./components/Sidebar";
+
+import "./global.css";
 
 export function App() {
   return (
---
+++ src/components/Post.jsx
@@ -0,0 +1,38 @@
+import styles from "./Post.module.css";
+export function Post() {
+  return (
+    <article className={styles.post}>
+      <header>
+        <div className={styles.author}>
+          <img
+            className={styles.avatar}
+            src="https://github.com/glemartello.png"
+          />
+          <div className={styles.authorInfo}>
+            <strong>Glenda Martello</strong>
+            <span>Web Developer</span>
+          </div>
+        </div>
+
+        <time title="18 de outubro às 15:40h" dateTime="2022-10-18 15:40:25">
+          Publicado há 1h
+        </time>
+      </header>
+
+      <div className={styles.content}>
+        <p>Fala galeraa 👋</p>
+        <p>
+          Acabei de subir mais um projeto no meu portifa. É um projeto que fiz
+          no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀
+        </p>
+        <p>
+          <a href=""> jane.design/doctorcare </a>
+        </p>
+        <p>
+          <a href="">#novoprojeto</a> <a href="">#nlw</a>{" "}
+          <a href="">#rocketseat</a>
+        </p>
+      </div>
+    </article>
+  );
+}
